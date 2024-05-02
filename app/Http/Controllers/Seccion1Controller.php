ebf8cfa397eed5df5558f88d441cfff05632647f
NeroArt <pacto.polar88@gmail.com>
2024-05-01 19:06:24 -0600

    feat(seccion1controller) Se añadio seguimiento en store

--- app/Http/Controllers/Seccion1Controller.php
+++ app/Http/Controllers/Seccion1Controller.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Models\actividades_avance;
 use App\Http\Controllers\Controller;
 use App\Models\curso;
 use Illuminate\Http\Request;
@@ -47,7 +47,247 @@ class Seccion1Controller extends Controller
         ->where('cursos.users_id', Auth::id())
         ->latest('id')
         ->value('id');
-        
+
+        $seccion1 = array(
+            "seccion" => "Sección 1",
+            "porcentaje_seccion" => 100,
+            "status" => 1,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion1);
+
+        $seccion2 = array(
+            "seccion" => "Sección 2",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion2);
+
+        $seccion3a = array(
+            "seccion" => "Sección 3A",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion3a);
+
+        $seccion3b = array(
+            "seccion" => "Sección 3B",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion3b);
+
+        $seccion3c = array(
+            "seccion" => "Sección 3C",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion3c);
+
+        $seccion3d = array(
+            "seccion" => "Sección 3D",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion3d);
+
+        $seccion4 = array(
+            "seccion" => "Sección 4",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion4);
+
+        $seccion5 = array(
+            "seccion" => "Sección 5",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion5);
+
+        $seccion6 = array(
+            "seccion" => "Sección 6",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion6);
+
+        $seccion7a = array(
+            "seccion" => "Sección 7A",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion7a);
+
+        $seccion7b = array(
+            "seccion" => "Sección 7B",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion7b);
+
+        $seccion7c = array(
+            "seccion" => "Sección 7C",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion7c);
+
+        $seccion7d = array(
+            "seccion" => "Sección 7D",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion7d);
+
+        $seccion8a = array(
+            "seccion" => "Sección 8A",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion8a);
+
+        $seccion8b = array(
+            "seccion" => "Sección 8B",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion8b);
+
+        $seccion8c = array(
+            "seccion" => "Sección 8C",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion8c);
+
+        $seccion8d = array(
+            "seccion" => "Sección 8D",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion8d);
+
+        $seccion8e = array(
+            "seccion" => "Sección 8E",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion8e);
+
+        $seccion8f = array(
+            "seccion" => "Sección 8F",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion8f);
+
+        $seccion9a = array(
+            "seccion" => "Sección 9A",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion9a);
+
+        $seccion9b = array(
+            "seccion" => "Sección 9B",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion9b);
+
+        $seccion9c = array(
+            "seccion" => "Sección 9C",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion9c);
+
+        $seccion9d = array(
+            "seccion" => "Sección 9D",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion9d);
+
+        $seccion9e = array(
+            "seccion" => "Sección 9E",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion9e);
+
+        $seccion9f = array(
+            "seccion" => "Sección 9F",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion9f);
+
+        $seccion9g = array(
+            "seccion" => "Sección 9G",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion9g);
+
+        $seccion9h = array(
+            "seccion" => "Sección 9H",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion9h);
+
+        $seccion9i = array(
+            "seccion" => "Sección 9I",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion9i);
+
+        $seccion9j = array(
+            "seccion" => "Sección 9J",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion9j);
+
+        $seccion9k = array(
+            "seccion" => "Sección 9K",
+            "porcentaje_seccion" => 0,
+            "status" => 0,
+            "cursos_id" => $idCurso
+        );
+        actividades_avance::insert($seccion9k);
+      
         //var_dump($idCurso);
         return redirect('seccion2/create')->with('idCurso',$idCurso); 
         
