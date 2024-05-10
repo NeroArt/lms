bd838b4074ee4ac62fc9712fd2ea05c1f9868317
george <georgeutm2014@gmail.com>
2024-05-10 00:34:40 -0600

    test de vite-config

---
+++ public/build/assets/scripts-DMlU6Kfa.js
@@ -0,0 +1,5 @@
+/*!
+* Start Bootstrap - Grayscale v7.0.6 (https://startbootstrap.com/theme/grayscale)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+*/window.addEventListener("DOMContentLoaded",o=>{var n=function(){const e=document.body.querySelector("#mainNav");e&&(window.scrollY===0?e.classList.remove("navbar-shrink"):e.classList.add("navbar-shrink"))};n(),document.addEventListener("scroll",n),document.body.querySelector("#mainNav")&&new bootstrap.ScrollSpy(document.body,{target:"#mainNav",rootMargin:"0px 0px -40%"});const a=document.body.querySelector(".navbar-toggler");[].slice.call(document.querySelectorAll("#navbarResponsive .nav-link")).map(function(e){e.addEventListener("click",()=>{window.getComputedStyle(a).display!=="none"&&a.click()})})});
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/sass/app.scss',
                 'resources/js/app.js',
                 'resources/css/styles.css',
+                'resources/js/scripts.js',
             ],
             refresh: true,
         }),
