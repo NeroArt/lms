5b5818ce94b2d17b62c344ccd9db7cf39632c3c6
george <georgeutm2014@gmail.com>
2024-05-10 00:30:42 -0600

    assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite(['resources/css/styles.css','resources/assets'])
+        @vite(['resources/css/styles.css'])
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Centro Evaluador de Competencias</title>
